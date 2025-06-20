22f276edfed919575e9e78480c840df5849680a7
Patricio Li <a1826991@adelaide.edu.au>
2025-06-20 03:08:30 +0000

    [automatic commit] 2025-06-20 03:08:29 UTC 1.158.225.90

--- part1/dogwalk-api/app.js
+++ part1/dogwalk-api/app.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
-app.get
+app.get('/api/dogs', async)
 
 
 module.exports = app;
