16250b0351107bd40f6d3cad585d612f61df730c
Patricio Li <a1826991@adelaide.edu.au>
2025-06-20 01:46:52 +0000

    [automatic commit] 2025-06-20 01:46:52 UTC 1.158.225.90

--- part1/part1_q5.sql
+++ part1/part1_q5.sql
@@ -21,6 +21,6 @@ INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, st
 VALUES
 ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
-((SELECT dog_id FROM Dogs WHERE name = 'Rocky'), '2025-06-10 08:00:00', 60, 'City Park', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'Daisy'), '2025-06-10 08:00:00', 35, 'Hillside Trail', 'cancelled'),
-((SELECT dog_id FROM Dogs WHERE name = 'Milo'), '2025-06-10 08:00:00', 40, 'Riverbank Walk', 'completed'),
+((SELECT dog_id FROM Dogs WHERE name = 'Rocky'), '2025-06-11 10:00:00', 60, 'City Park', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Daisy'), '2025-06-12 11:00:00', 35, 'Hillside Trail', 'cancelled'),
+((SELECT dog_id FROM Dogs WHERE name = 'Milo'), '2025-06-12 13:00:00', 40, 'Riverbank Walk', 'completed'),
