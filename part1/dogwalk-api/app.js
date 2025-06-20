a0fb6d4803e01f77d08926b231c6d5901264fc85
Patricio Li <a1826991@adelaide.edu.au>
2025-06-20 03:21:37 +0000

    [automatic commit] 2025-06-20 03:21:37 UTC 1.158.225.90

--- part1/dogwalk-api/app.js
+++ part1/dogwalk-api/app.js
@@ -52,7 +52,11 @@ app.get('/api/walkrequests/open', async (req, res) => {
             FROM WalkRequests walkrequest
             JOIN Dogs dog ON walkrequest.dog_id = dog.dog_id
             JOIN Users user ON dog.owner_id = user.user_id
-            WHERE walkrequest.status = ''`)
+            WHERE walkrequest.status = 'open
+        '`)
+        res.json(rows);
+    } catch (err) {
+        console.error
     }
 })
 
