d110967702a6c1e5f518f1aa640fbd23a981c21e
Patricio Li <a1826991@adelaide.edu.au>
2025-06-21 00:40:43 +0000

    [automatic commit] 2025-06-21 00:40:43 UTC 1.158.225.90

--- part2/app.js
+++ part2/app.js
@@ -24,7 +24,8 @@ app.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
     if (!username || !password) {
-        return res.status(400).json
+        return res.status(400).json({ message: 'Missing username or password' });
+        
     }
 })
 
